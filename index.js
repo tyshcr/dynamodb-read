d55b7f463c673977021c0d06cbf3023934c71ef1
Christopher Tysh <chris@feltpad.net>
2017-04-27 21:32:18 -0500

    Changes to format of outgoing JSON

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var AWS = require("aws-sdk");
+let AWS = require("aws-sdk");
 
 exports.handler = function(event, context, callback) {
-    var docClient = new AWS.DynamoDB.DocumentClient();
+    let docClient = new AWS.DynamoDB.DocumentClient();
 
-    var params = {
+    let params = {
         "TableName" : "mlb",
         "FilterExpression" : "conference = :conf and division = :div",
         "ExpressionAttributeValues" : {":conf": "AL", ":div": "C"}
     };
 
-    var output = docClient.scan(params, function(err, data) {
+    let output = docClient.scan(params, function(err, data) {
         if (err) {
-            // console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
             callback(err, "Query Failed")
         } else {
           data.Items.sort(function(a, b) {
                 return a.rank > b.rank
-            })
-            // console.log(JSON.stringify(data))
-            callback(null, data)
+          })
+
+          let json = { "title": "MLB", "segments":[{ "name":"AL Central", "rows": data.Items }] }
+
+          callback(null, json)
         }
     });
 
